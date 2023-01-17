1feebb11ce081f9ab54e0e7b0c5399a3259179f4
auberr <auberr@daum.net>
2023-01-17 23:24:55 +0900

    update some

--- dsandalgo/everyones_algorithm/008_selectionsort.py
+++ dsandalgo/everyones_algorithm/008_selectionsort.py
@@ -17,17 +17,34 @@
 # d=[2,4,5,1,3]
 # print(sel_sort(d))
 
-def select_sort(a):
+def select_sort_ascending(a):
     n = len(a)
     for i in range(0, n-1):
         min_idx = i
         for j in range(i+1, n):
-            if a[j] < a[min_idx]:
+            if a[j] < a[i]:
                 min_idx = j
                 a[i], a[min_idx] = a[min_idx], a[i]
                 print(a[i], a[min_idx])
 
 
 d=[2, 4, 5, 1, 3]
-print(select_sort(d))
+print(select_sort_ascending(d))
+print(d)
+
+
+
+def select_sort_descending(a):
+    n = len(a)
+    for i in range(0, n-1):
+        min_idx = i
+        for j in range(i+1, n):
+            if a[j] > a[i]:
+                min_idx = j
+                a[i], a[min_idx] = a[min_idx], a[i]
+                print(a[i], a[min_idx])
+
+
+d=[2, 4, 5, 1, 3]
+print(select_sort_descending(d))
 print(d)
