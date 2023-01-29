239d01cfaef48436c106914218e826d412172aec
auberr <auberr@daum.net>
2023-01-29 19:26:22 +0900

    update some

--- dsandalgo/studynote/everyones_algorithm/008_select.py
+++ dsandalgo/studynote/everyones_algorithm/008_select.py
@@ -3,15 +3,18 @@ def select_sort(a):
     n = len(a)
     for i in range(0, n-1):
         min_idx = i
-
         print(min_idx, '--')
         for j in range(i+1, n):
-            print(min_idx, '----')
             print(j, '----')
             if a[j] < a[min_idx]:
-                print(a[i], a[min_idx], a[j])
+                print(i, min_idx, j)
+                print('실제수', a[i], a[min_idx], a[j])
                 min_idx = j
                 a[i], a[min_idx] = a[min_idx], a[i]
+                min_idx = i
+
+                print(a)
+                
                 
 
 
